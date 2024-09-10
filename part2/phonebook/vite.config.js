27dcc544db039ae33d8d25f5de72cb2f41ac071a
jaszmew <jaszmew@gmail.com>
2024-09-10 10:59:02 +0300

    test changes

--- part2/phonebook/vite.config.js
+++ part2/phonebook/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3001",
+        target: "https://fullstackopen-6b8m.onrender.com",
         changeOrigin: true,
       },
     },
